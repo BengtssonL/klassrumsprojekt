1d91234638a3c071584ee72c194eb075eb992139
Lucas Bengtsson <lucas.bengtsson77@gmail.com>
2023-03-15 14:59:15 +0100

    Add project files.

---
+++ KlassrumsProjekt/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KlassrumsProjekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void eventLog1_EntryWritten(object sender, System.Diagnostics.EntryWrittenEventArgs e)
+        {
+
+        }
+       
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(0, 10);
+
+            button1.Size = new Size(button1.Width - a, button1.Height);
+              
+
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(0, 10);
+
+            button2.Size = new Size(button2.Width - a, button2.Height);
+
+
+        }
+    }
+}
